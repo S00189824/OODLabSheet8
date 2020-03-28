fae6bc35ce62da4526f9581ec985f314f3802f01
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-03-28 13:43:17 +0000

    dfg

---
+++ Labsheet8/DataManagement/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Labsheet8;
+
+namespace DataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TeamData db = new TeamData();
+
+            using (db)
+            {
+                Team t1 = new Team() { TeamID = 1, TeamName = "Ballina Town FC", Location = "Ballina" };
+                Player p1 = new Player() { PlayerID = 1, Name = "Dave", Position = "Forward", TeamID = 1, team = t1 };
+                Player p2 = new Player() { PlayerID = 2, Name = "Mick", Position = "Defender", TeamID = 1, team = t1 };
+
+                Team t2 = new Team() { TeamID = 1, TeamName = "Sligo Rovers", Location = "Sligo" };
+                Player p3 = new Player() { PlayerID = 1, Name = "Tom", Position = "GoalKeeper", TeamID = 2, team = t2 };
+                Player p4 = new Player() { PlayerID = 2, Name = "Harry", Position = "Defender", TeamID = 2, team = t2 };
+
+                db.teams.Add(t1);
+                db.teams.Add(t2);
+
+                Console.WriteLine("Added Teams to database ");
+
+                db.players.Add(p1);
+                db.players.Add(p2);
+                db.players.Add(p3);
+                db.players.Add(p4);
+
+                Console.WriteLine("Added players to database");
+
+                db.SaveChanges();
+
+                Console.WriteLine("Saved to database");
+            }
+        }
+    }
+}
---
+++ Labsheet8/Labsheet8/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet8
+{
+    public class Player
+    {
+        public int PlayerID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public int TeamID { get; set; }
+        public virtual Team team { get; set; }
+    }
+
+    public class Team
+    {
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+        public string Location { get; set; }
+
+        public virtual List<Player> players { get; set; }
+    }
+
+    public class TeamData : DbContext
+    {
+        public TeamData() : base("MyTeamData") { }
+
+        public DbSet<Player> players { get; set; }
+        public DbSet<Team> teams { get; set; }
+    }
+}
